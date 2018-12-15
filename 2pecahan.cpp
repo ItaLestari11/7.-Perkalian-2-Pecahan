75a21b766f746a02983d92ad2b8f5b2ea2b50b43
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:46:23 +0700

    Add files via upload

---
+++ 2pecahan.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int hasil;
+
+cout<<"Program perkalian 2 buah pecahan"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+
+cout<<"P1= 3.0/4.0"<<endl;
+cout<<"P2= 2.0/3.0"<<endl;
+
+hasil=(3.0*2.0)/(3.0*4.0);
+cout<<"Maka hasilnya adalah:"<<hasil<<endl;
+return 0;
+}
